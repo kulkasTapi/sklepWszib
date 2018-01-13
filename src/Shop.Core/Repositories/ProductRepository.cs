a6abfa0e4c2d875edbd6d34a3e71232710ff71f5
Aleksander Józef Żur <azur@student.wszib.edu.pl>
2018-01-13 16:52:37 +0100

    dużo zmian mająych na celu oddizelenie UI od logiki biznesowej znajdującej się w Core, transport danych odbywa się przez DTO

---
+++ src/Shop.Core/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.Core.Domain;
+using System.Linq;
+
+namespace Shop.Core.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ISet<Product> _products = new HashSet<Product>()
+        {
+            new Product("Laptop", "Electronics", 3500),
+            new Product("Jeans", "Trousers", 150),
+            new Product("Hummer", "Tools", 50)
+        };
+        public Product Get(Guid id) => _products.SingleOrDefault(x => x.Id == id);
+
+        public IEnumerable<Product> GetAll() => _products;
+
+        public void Add(Product product) => _products.Add(product);
+
+    }
+}
--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -5,30 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
 using Shop.Web.Models;
+using Shop.Core.Repositories;
+using Shop.Core.Services;
 
 namespace Shop.Web.Controllers
 {
     [Route("products")]
     public class ProductsController : Controller
     {
-        private static readonly List<Product> _products = new List<Product>
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
         {
-            new Product("Laptop", "Electronics", 3500),
-            new Product("Jeans", "Trousers", 150),
-            new Product("Hummer", "Tools", 50)
-        };
+            _productService = productService;
+        }
 
         [HttpGet]
         public IActionResult Index()
         {
-            var products = _products.Select(p => new ProductViewModel
+            var products = _productService
+                .GetAll()
+                .Select(p => new ProductViewModel
             {
                 Id = p.Id,
                 Name = p.Name,
                 Category = p.Category,
                 Price = p.Price
             });
-            return View(_products);
+            return View(products);
         }
 
         [HttpGet("add")]
@@ -46,7 +49,7 @@ namespace Shop.Web.Controllers
             {
                 return View(viewModel);
             }
-            _products.Add(new Product(viewModel.Name, viewModel.Category, viewModel.Price));
+            _productService.Add(viewModel.Name, viewModel.Category, viewModel.Price);
 
             return RedirectToAction(nameof(Index));
         }

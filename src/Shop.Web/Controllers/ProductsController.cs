ebe14bec7a911824c2575e67f3e8c8331401e9f3
Aleksander Józef Żur <azur@student.wszib.edu.pl>
2018-01-13 15:04:41 +0100

    jest dodawanie produktów do listy

--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
+using Shop.Web.Models;
 
 namespace Shop.Web.Controllers
 {
+    [Route("products")]
     public class ProductsController : Controller
     {
         private static readonly List<Product> _products = new List<Product>
@@ -16,9 +18,30 @@ namespace Shop.Web.Controllers
             new Product("Hammer", "Tools", 50)
         };
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_products);
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            var viewModel = new ProductViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(ProductViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            _products.Add(new Product(viewModel.Category, viewModel.Name, viewModel.Price));
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ src/Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Models
+{
+    public class ProductViewModel
+    {
+        //utwóz nowy produkt bazując na dropdawn pod 
+        [Required(ErrorMessage = "Name was not provided.")]
+        [StringLength(100, MinimumLength = 3)]//opcjonalnie jest ilość minimalna
+        public string Name { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        //lista która służy do pobierania kategorize słownika SelectListItem
+        public List<SelectListItem> Categories { get; } = new List<SelectListItem>
+        {
+          new SelectListItem {Text=  "Electronics", Value =  "Electronics"},
+          new SelectListItem {Text=  "Trousers", Value =  "Trousers"},
+          new SelectListItem {Text=  "Tools", Value =  "Tools"}
+        };
+    }
+}
